92ced977461a90d19a3b7a48827e9e45e2c9091f
Eddie Wang <eddie.wang0809@outlook.com>
2024-02-02 11:59:30 +0800

    Movement physics - X axis

--- life_on_land/player.py
+++ life_on_land/player.py
@@ -1,10 +1,15 @@
+from typing import TYPE_CHECKING
+
 import arcade
 
 from life_on_land.constants import *
 
+if TYPE_CHECKING:
+    from life_on_land.game import GameWindow
+
 
 class PlayerSprite(arcade.Sprite):
-    def __init__(self, game_window):
+    def __init__(self, game_window: "GameWindow"):
         super().__init__(
             scale=2,
         )
@@ -13,5 +18,30 @@ class PlayerSprite(arcade.Sprite):
 
         self.game_window = game_window
 
+        self.PLAYER_X_VEL = 0
+        self.VEL_DIFFERENCE = 0
+        self.TARGET_VEL = 0
+        self.FRICTION_FACTOR = 0.3
+
     def on_update(self, delta_time: float = 1 / 60):
-        pass
+        right_pressed = InputType.RIGHT in self.game_window.pressed_inputs
+        left_pressed = InputType.LEFT in self.game_window.pressed_inputs
+
+        self.TARGET_VEL = 0
+        if right_pressed and left_pressed:
+            self.TARGET_VEL = 0
+        elif right_pressed:
+            self.TARGET_VEL = 5
+        elif left_pressed:
+            self.TARGET_VEL = -5
+
+        self.VEL_DIFFERENCE = self.TARGET_VEL - self.PLAYER_X_VEL
+        self.PLAYER_X_VEL += self.VEL_DIFFERENCE * self.FRICTION_FACTOR
+
+        self.change_x = self.PLAYER_X_VEL
+
+        if (
+            self.game_window.is_buffered(InputType.UPSSSS)
+            and self.game_window.engine.can_jump()
+        ):
+            self.game_window.consume_buffer(InputType.UP)
