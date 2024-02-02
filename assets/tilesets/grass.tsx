808ee54f78bb01952cdac50becd0604027987ff7
Max Lin <Linmax119@gmail.com>
2024-02-02 14:42:41 +0800

    Assets and incomplete lv1

---
+++ assets/tilesets/grass.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="grass" tilewidth="32" tileheight="32" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../textures/GRASS/GRASS.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../textures/GRASS/GRASS-NOTOP.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../textures/GRASS/wood.png"/>
+ </tile>
+</tileset>
