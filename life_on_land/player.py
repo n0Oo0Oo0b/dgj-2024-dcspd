5105d7c13e67b4d322e07d6262f120528d127040
Daniel Gu <bobthebuilder10492@gmail.com>
2024-02-02 14:51:31 +0800

    New special effect system (#10)
    * Add jumping
    
    * Fix whitespace
    
    * Create interface for level-specific effects
    
    * Add null level effect
    
    * Use new special effect system
    
    * Refactor constants to class attributes

--- life_on_land/player.py
+++ life_on_land/player.py
@@ -3,18 +3,18 @@ from typing import TYPE_CHECKING
 import arcade
 
 from life_on_land.constants import *
+from life_on_land.level_effects import *
 
 if TYPE_CHECKING:
     from life_on_land.game import GameWindow
 
 
-FRICTION_FACTOR = 0.4
-PLAYER_SPEED = 5
-PLAYER_JUMP_FORCE = 10
-COYOTE_DURATION = 0.1
-
-
 class PlayerSprite(arcade.Sprite):
+    FRICTION_FACTOR = 0.4
+    PLAYER_SPEED = 5
+    PLAYER_JUMP_FORCE = 10
+    COYOTE_DURATION = 0.1
+
     def __init__(self, game_window: "GameWindow"):
         super().__init__(
             scale=2,
@@ -24,8 +24,7 @@ class PlayerSprite(arcade.Sprite):
 
         self.last_grounded: float = -1
         self.game_window: "GameWindow" = game_window
-
-        self.level_1_unlocked = True
+        self.special = FireHoseEffect(self)
 
     def on_update(self, delta_time: float = 1 / 60):
         # Update attributes
@@ -36,25 +35,17 @@ class PlayerSprite(arcade.Sprite):
         # X movement
         right_pressed = InputType.RIGHT in game.pressed_inputs
         left_pressed = InputType.LEFT in game.pressed_inputs
-        target_vel = (right_pressed - left_pressed) * PLAYER_SPEED
+        target_vel = (right_pressed - left_pressed) * self.PLAYER_SPEED
 
         vel_diff = target_vel - self.velocity[0]
-        self.velocity[0] += vel_diff * FRICTION_FACTOR
+        self.velocity[0] += vel_diff * self.FRICTION_FACTOR
 
         # Y movement (jump)
         if (
             game.is_buffered(InputType.UP)
-            and self.last_grounded + COYOTE_DURATION >= game.global_time
+            and self.last_grounded + self.COYOTE_DURATION >= game.global_time
         ):
             game.consume_buffer(InputType.UP)
-            self.velocity[1] = PLAYER_JUMP_FORCE
+            self.velocity[1] = self.PLAYER_JUMP_FORCE
 
-        if self.level_1_unlocked:
-            self.level_1_special()
-
-    def level_1_special(self):
-        game = self.game_window
-        if game.is_buffered(InputType.SPECIAL):
-            game.consume_buffer(InputType.SPECIAL)
-            print("Special Activated")
-            self.velocity[1] = 15
+        self.special.on_update()
